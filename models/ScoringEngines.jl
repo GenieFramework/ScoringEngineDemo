67a7d52f47ab17573fa5836450346e95a71ca248
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-07-04 14:31:01 +0530

    Adding model fields

--- models/ScoringEngines.jl
+++ models/ScoringEngines.jl
@@ -9,8 +9,12 @@ export ScoringEngine
 @reactive mutable struct ScoringEngine <: ReactiveModel
   features::R{Vector{String}} = ["pol_no_claims_discount", "pol_duration", "pol_sit_duration", "vh_value",
   "vh_weight", "vh_age", "population", "town_surface_area", "drv_age1", "drv_age_lic1"]
-
   feature::R{String} = "vh_value"
+
+  groupmethods::R{Vector{String}} = ["quantiles", "linear"]
+  groupmethod::R{String} = "quantiles"
+
+  sample_size::R{Int} = 50
 end
 
 function handlers(model::ScoringEngine)
